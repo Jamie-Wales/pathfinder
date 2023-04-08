2084817ebe752f2b18e139be99eb565d49ea126f
Jamie Wales <jamie.wales.developer@gmail.com>
2023-04-08 12:18:51 +0100

    Drag and draw!

--- Display.java
+++ Display.java
@@ -1,15 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
 import java.util.Arrays;
 
 
 public class Display extends JFrame {
 
-    private JPanel[][] grid;
-    private Boolean[][] clicked;
+    private final JPanel[][] grid;
+    private final Boolean[][] clicked;
 
     public Display(int rows, int cols) {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -20,15 +19,17 @@ public class Display extends JFrame {
 
         grid = new JPanel[rows][cols];
         clicked = new Boolean[rows][cols];
-        for (int i = 0; i < clicked.length; i++) {
-            Arrays.fill(clicked[i], false);
+        for (Boolean[] booleans : clicked) {
+            Arrays.fill(booleans, false);
         }
         setLayout(new GridLayout(rows, cols));
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 grid[i][j] = new JPanel();
                 grid[i][j].setBackground(Color.GRAY);
-                grid[i][j].addMouseListener(new SquareListener());
+                SquareListener squareListener = new SquareListener();
+                grid[i][j].addMouseListener(squareListener);
+                grid[i][j].addMouseMotionListener(squareListener);
                 grid[i][j].setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
                 add(grid[i][j]);
             }
@@ -38,42 +39,21 @@ public class Display extends JFrame {
 
     }
 
-    private class SquareListener implements MouseListener, MouseMotionListener {
-
-        private boolean pressed = false;
-        @Override
-        public void mouseClicked(MouseEvent e) {
-            JPanel square = (JPanel) e.getSource();
-            int cols = colIndex(square);
-            int row = rowIndex(square);
-            if (!clicked[row][cols]) {
-                square.setBackground(Color.black);
-                clicked[row][cols] = true;
-            } else {
-                square.setBackground(Color.GRAY);
-                clicked[row][cols] = false;
-            }
-
-
-
-        }
+    private class SquareListener extends MouseAdapter {
 
         @Override
         public void mousePressed(MouseEvent e) {
-            pressed = true;
+            processMouseEvent(e);
         }
-
-        @Override
-        public void mouseReleased(MouseEvent e) {
-            pressed = false;
-        }
-
 
         @Override
         public void mouseEntered(MouseEvent e) {
-            JPanel square = (JPanel) e.getSource();
-            square.setBackground(Color.red);
-
+            if (e.getModifiersEx() == MouseEvent.BUTTON1_DOWN_MASK) {
+                processMouseEvent(e);
+            } else {
+                JPanel square = (JPanel) e.getSource();
+                square.setBackground(Color.red);
+            }
         }
 
         @Override
@@ -86,17 +66,19 @@ public class Display extends JFrame {
             } else {
                 square.setBackground(Color.black);
             }
-
         }
 
-        @Override
-        public void mouseDragged(MouseEvent e) {
-
-        }
-
-        @Override
-        public void mouseMoved(MouseEvent e) {
-
+        private void processMouseEvent(MouseEvent e) {
+            JPanel square = (JPanel) e.getSource();
+            int cols = colIndex(square);
+            int row = rowIndex(square);
+            if (!clicked[row][cols]) {
+                square.setBackground(Color.black);
+                clicked[row][cols] = true;
+            } else {
+                square.setBackground(Color.GRAY);
+                clicked[row][cols] = false;
+            }
         }
     }
 
--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Display display = new Display(30, 30);
+
+        Display display = new Display(30,  30);
     }
 }
